55311a6dbb2b698897a00994a1135679297f50c6
Omomayorwah <kintunde2010@gmail.com>
2025-03-12 18:08:38 +0100

    Update assingment.py

--- assingment.py
+++ assingment.py
@@ -1,6 +1,6 @@
-first_number =float(input("First Number"))
-second_number = float(input("Second Number"))
-operation = input("Arithmetic Operation +, -, *, or /")
+first_number =float(input("First Number "))
+second_number = float(input("Second Number "))
+operation = input("Arithmetic Operation +, -, *, or / ")
 if operation == "+":
   print(first_number, " + ", second_number, " = ", first_number + second_number)
 elif operation == "-":
